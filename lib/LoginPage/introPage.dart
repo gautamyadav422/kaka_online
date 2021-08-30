d4800ff7d29ae7a22e882624fe02e1daaee7e0ed
gautamyadav422 <gautamyadav422@gmail.com>
2021-08-30 20:22:33 +0530

    git add .

--- lib/LoginPage/introPage.dart
+++ lib/LoginPage/introPage.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:intro_slider/slide_object.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  List<Slide> slides = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    slides.add(Slide(
+
+
+    ),);
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
